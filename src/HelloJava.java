89bbfea8e3a3eb861c8a29464fb0c12c13a801a8
김동진 <dongjin403@naver.com>
2024-03-16 14:25:48 +0900

    master삭제성공~~

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -6,6 +6,7 @@ public class HelloJava {
         System.out.println("hello java");
         System.out.println("hello java");
         System.out.println("hello java");
+        System.out.println("깃 마스터 브랜치 삭제 성공");
     }
 
 }
