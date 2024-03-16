020a788f840dd5422b5ec54f67e6bdc49dd611ec
김동진 <dongjin403@naver.com>
2024-03-16 14:24:10 +0900

    master삭제

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -5,6 +5,7 @@ public class HelloJava {
         System.out.println("hello java");
         System.out.println("hello java");
         System.out.println("hello java");
+        System.out.println("hello java");
     }
 
 }
