7451173426a4bd946d11303f2105f0cdb69695e1
김동진 <dongjin403@naver.com>
2024-03-19 23:57:40 +0900

    문자열압축

---
+++ src/ch1/Main.java
@@ -0,0 +1,21 @@
+package ch1;
+
+import java.util.Scanner;
+
+/**
+ * 암호
+ */
+class Main {
+	public String solution(String str, char c) {
+		String answer = "";
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		char c = kb.next().charAt(0);
+		System.out.println(T.solution(str, c));
+	}
+}
