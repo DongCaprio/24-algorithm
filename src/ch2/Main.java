37656d4ea6f434fd236252290d46ae027bbee4da
김동진 <dongjin403@naver.com>
2024-03-21 21:04:37 +0900

    가위바위보

--- src/ch2/Main.java
+++ src/ch2/Main.java
@@ -3,16 +3,19 @@ package ch2;
 import java.util.Scanner;
 
 class Main {
-	public String solution(String str, char c) {
+	public String solution(int n) {
 		String answer = "";
+
+
+
+
 		return answer;
 	}
 	
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
-		String str = kb.next();
-		char c = kb.next().charAt(0);
-		System.out.println(T.solution(str, c));
+		int n = kb.nextInt();
+		System.out.println(T.solution(n));
 	}
 }
