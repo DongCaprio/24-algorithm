6e9ebaf3b4572a3e114b912e3673e90ab4ccc952
김동진 <dongjin403@naver.com>
2024-03-24 15:07:13 +0900

    멘토링

---
+++ src/ch3/Main.java
@@ -0,0 +1,18 @@
+package ch3;
+
+import java.util.Scanner;
+
+class Main {
+	public String solution(String str, char c) {
+		String answer = "";
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		char c = kb.next().charAt(0);
+		System.out.println(T.solution(str, c));
+	}
+}
