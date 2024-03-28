070494a73bb517416a6ef272b83e26d4c51de547
김동진 <dongjin403@naver.com>
2024-03-28 20:22:52 +0900

    v

--- src/ch5/Main.java
+++ src/ch5/Main.java
@@ -1,35 +1,18 @@
-package ch4;
+package ch5;
 
 import java.util.Scanner;
 
 class Main {
-	public int solution(String s, String t) {
-		int answer = 0;
-
-		int n = t.length();
-		for(int i =0; i<=s.length()-n;i++){
-			String str = s.substring(i,i+n);
-			char[] chars = str.toCharArray();
-			Arrays.sort(chars);
-			str = new String(chars);
-
-			System.out.println(str);
-		}
-
-
-
-
-
-
-
+	public String solution(String str, char c) {
+		String answer = "";
 		return answer;
 	}
 	
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
-		String s = kb.next();
-		String t = kb.next();
-		System.out.println(T.solution(s, t));
+		String str = kb.next();
+		char c = kb.next().charAt(0);
+		System.out.println(T.solution(str, c));
 	}
 }
