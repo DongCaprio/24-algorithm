8566058ce342daab9bb7776ecf3f9c0acfcba694
김동진 <dongjin403@naver.com>
2024-03-29 09:03:31 +0900

    Create Main.java

---
+++ src/ch5/Main.java
@@ -0,0 +1,19 @@
+package ch5;
+import java.util.Scanner;
+
+class Main {
+	public int solution(String str) {
+		int answer = 0;
+		
+		
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String str = kb.next();
+		System.out.println(T.solution(str));
+	}
+}
