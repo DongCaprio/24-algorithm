fc306efe3dd04257aba20259ccf9082ec8664b8b
김동진 <dongjin403@naver.com>
2024-04-03 00:07:00 +0900

    .

--- src/ch6/Main.java
+++ src/ch6/Main.java
@@ -1,18 +1,14 @@
 package ch6;
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 class Main {
-	public String solution(int n, int[][] arr) {
-		String answer = "";
-		Arrays.sort(arr, (o1,o2) -> (o1[0] == o2[0] ? o1[1] - o2[1] : o1[0] - o2[0]));
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<arr[i].length; j++) {
-				System.out.print(arr[i][j]+" "); 
-			}
-			System.out.println();
-		}		
-		
+	public int solution(int n, int m, int[] arr) {
+		int answer = 0;
+
+
+
+
 		return answer;
 	}
 	
@@ -20,12 +16,15 @@ class Main {
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
 		int n = kb.nextInt();
-		int[][] arr = new int[n][2];
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<arr[i].length; j++) {
-				arr[i][j] = kb.nextInt();
-			}
-		}		
-		System.out.println(T.solution(n, arr));
+		int m = kb.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) arr[i] = kb.nextInt();
+
+		System.out.println(T.solution(n,m,arr));
+	}
+
+
+	class aa{
+
 	}
 }
