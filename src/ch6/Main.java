57dad2d763cfe8b1eff099681d747715e98dfb57
김동진 <dongjin403@naver.com>
2024-04-07 23:28:23 +0900

    뮤직비디오 풀이

--- src/ch6/Main.java
+++ src/ch6/Main.java
@@ -1,15 +1,35 @@
 package ch6;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
+
+	public int count(int[] arr , int capacity){
+		int cnt = 1, sum=0;
+		for(int x : arr){
+			if(sum+x>capacity){
+				cnt++;
+				sum=x;
+			}else{
+				sum+=x;
+			}
+		}
+		return cnt;
+	}
 	public int solution(int n, int m, int[] arr) {
 		int answer = 0;
-		int answer2 = 0;
-		int answer3 = 0;
-		int answer4 = 0;
-		int answer5 = 0;
-
+		int lt= Arrays.stream(arr).max().getAsInt();
+		int rt= Arrays.stream(arr).sum();
+		while(lt<=rt){
+			int mid = (lt+rt)/2;
+			if(count(arr, mid)<=m) {
+				answer =mid;
+				rt=mid-1;
+			}else{
+				lt=mid+1;
+			}
+		}
 
 
 
@@ -25,10 +45,5 @@ class Main {
 		for(int i=0; i<n; i++) arr[i] = kb.nextInt();
 
 		System.out.println(T.solution(n,m,arr));
-	}
-
-
-	class aa{
-
 	}
 }
