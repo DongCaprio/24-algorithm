5dae95eed65ca79540af5afacabbc0b915075ecf
김동진 <dongjin403@naver.com>
2024-04-20 17:53:29 +0900

    DFS

---
+++ src/ch6/MainBFS.java
@@ -0,0 +1,45 @@
+package ch6;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class MainBFS {
+
+	int answer = 0;
+	int[] dis = {1, -1, 5};
+	int[] ch;
+	Queue<Integer> Q = new LinkedList<>();
+
+	public int BFS(int s, int e) {
+		ch = new int[10001];
+		ch[s] =1;
+		Q.offer(s);
+		int L=0;
+		while (!Q.isEmpty()){
+			int len = Q.size();
+			for(int i=0; i<len; i++){
+				int x = Q.poll();
+				for(int j=0; j<dis.length; j++){
+					int nx = x + dis[j];
+					if(nx == e) return L+1;
+					if(nx >= 1 && nx <= 10000 && ch[nx] == 0){
+						ch[nx] = 1;
+						Q.offer(nx);
+					}
+				}
+			}
+			L++;
+		}
+
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		MainBFS T = new MainBFS();
+		Scanner kb = new Scanner(System.in);
+		int s = kb.nextInt();
+		int e = kb.nextInt();
+		System.out.println(T.BFS(s, e));
+	}
+}
--- src/ch6/Main경로탐색_인접행렬.java
+++ src/ch6/Main경로탐색_인접행렬.java
@@ -1,40 +1,36 @@
 package ch6;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
-class Main {
+class Main경로탐색_인접행렬 {
 
 	static int n, m, answer=0;
-	static ArrayList<ArrayList<Integer>> graph;
+	static int[][] graph;
 	static int[] ch;
 	public void DFS(int v) {
 		if(v==n) answer++;
 		else{
-			for(int nv : graph.get(v)){
-				if(ch[nv] ==0){
-					ch[nv] = 1;
-					DFS(nv);
-					ch[nv] = 0;
+			for(int i=1; i<=n; i++){
+				if(graph[v][i] == 1 && ch[i]==0){
+					ch[i] = 1;
+					DFS(i);
+					ch[i] = 0;
 				}
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		Main T = new Main();
+		Main경로탐색_인접행렬 T = new Main경로탐색_인접행렬();
 		Scanner kb = new Scanner(System.in);
 		n = kb.nextInt();
 		m = kb.nextInt();
-		graph = new ArrayList<ArrayList<Integer>>();
-		for(int i=0; i<=n; i++){
-			graph.add(new ArrayList<Integer>());
-		}
+		graph = new int[n+1][n+1];
 		ch=new int[n+1];
 		for(int i=0; i<m; i++){
 			int a = kb.nextInt();
 			int b = kb.nextInt();
-			graph.get(a).add(b);
+			graph[a][b] = 1;
 		}
 		ch[1] = 1;
 		T.DFS(1);
